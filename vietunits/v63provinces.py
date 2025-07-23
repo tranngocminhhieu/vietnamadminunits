a6fa2640de1de991f7064c18cc1f6da89ff2c162
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2025-07-23 16:02:36 +0700

    data_new_wards = []

---
+++ vietunits/v63provinces.py
@@ -0,0 +1,7 @@
+class Unit:
+    def __init__(self, address=None, province=None, district=None, ward=None):
+        self.address = address
+        self.province = province
+        self.district = district
+        self.ward = ward
+

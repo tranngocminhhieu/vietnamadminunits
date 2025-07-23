68d95642267f7c92dd109355e6d91c858ab7cdbe
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2025-07-23 15:37:53 +0700

    data_new_wards = []

---
+++ scrap_63_provinces_ward_location.py
@@ -0,0 +1,82 @@
+import json
+import sqlite3
+import time
+import warnings
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
+from seleniumbase import Driver
+
+warnings.filterwarnings('ignore')
+
+BASE_DIR = Path(__file__).resolve().parent
+
+
+def get_location(address, driver):
+    driver.send_keys('#query-input', address)
+    driver.click('#geocode-button')
+    driver.find_element('#query-input').clear()
+    time.sleep(10)  # Avoid rate limit and waiting for HTML is rendered
+    html = driver.get_page_source()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Result
+    results_display = soup.find(id='results-display-div')
+
+    # Location (Latitude, Longitude)
+    result_location = results_display.find(class_='result-location')
+    location = result_location.text.split()[1]
+
+    # Bounds
+    result_bounds = results_display.find(class_='result-bounds')
+    result_viewport = results_display.find(class_='result-viewport')
+    bounds_element = result_bounds or result_viewport
+    if bounds_element:
+        bounds = ' '.join(bounds_element.text.split()[1:])
+    else:
+        bounds = None
+
+    # Formated Address
+    result_formatted_address = results_display.find(class_='result-formatted-address')
+    if result_formatted_address:
+        formatted_address = result_formatted_address.text.split('\n')[-1].strip()
+    else:
+        formatted_address = None
+
+    data = {
+        'address': address,
+        'wardLat': location.split(',')[0],
+        'wardLon': location.split(',')[1],
+        'wardBounds': bounds,
+        'wardFormattedAddress': formatted_address,
+    }
+
+    return data
+
+if __name__ == '__main__':
+    df = pd.read_csv(BASE_DIR / 'data/danhmuc_and_sapnhap.csv')
+    df_63 = df[['province', 'district', 'ward']].drop_duplicates().reset_index(drop=True)
+    df_63['address'] = np.where(df_63['ward'].notna(),
+                                df_63['ward'].fillna('') + ', ' + df_63['district'] + ', ' + df_63['province'],
+                                df_63['district'] + ', ' + df_63['province'])
+
+    driver = Driver(uc=True)
+    driver.get('https://developers-dot-devsite-v2-prod.appspot.com/maps/documentation/utils/geocoder?hl=vi')
+
+    location_data = []
+    for index, row in df_63.iterrows():
+        address = row['address']
+        location = get_location(address, driver)
+        data = {
+            'address': address,
+            'location': json.dumps(location)
+        }
+        location_data.append(data)
+
+        if ((index + 1) % 15 == 0) or ((index + 1) == len(df_63)):
+            df_data = pd.DataFrame(location_data)
+            with sqlite3.connect(BASE_DIR / 'data/63_provinces_ward_location.db') as conn:
+                df_data.to_sql(name='location', con=conn, if_exists='append', index=False)
+            data_new_wards = []

e04367534a0fcb2a524ef5407a00ee9aa0974c53
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2025-08-04 16:26:03 +0700

    Change the way to store alias keywords

--- scripts/module_testing/testing.py
+++ scripts/module_testing/testing.py
@@ -1,4 +1,3 @@
 from vietnamadminunits import parse_address, convert_address
 
-# print(parse_address('nguyen thai binh, quan 1, ho chi minh', mode='LEGACY'))
-print(convert_address('phuong 14 quận 10 Thành Phố Hồ Chí Minh'))
+print(parse_address('phuong 10, quan 3, hcm', mode='LEGACY'))
